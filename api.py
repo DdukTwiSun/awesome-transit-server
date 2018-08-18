d860528fb427a24ab9c5948181fad959dea46882
Song Jaehak <master@hpi.cc>
2018-08-18 19:56:05 +0800

    Add firebse credentials

--- api.py
+++ api.py
@@ -2,6 +2,11 @@ import uuid
 import boto3
 import os
 from flask import Flask, request, jsonify
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
 app = Flask(__name__)
 
