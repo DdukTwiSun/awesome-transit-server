fe945d36ec10c9eb364eae0d9df8c500ffaf5518
Song Jaehak <master@hpi.cc>
2018-08-18 14:37:05 +0800

    Add face location

---
+++ camera.py
@@ -0,0 +1,32 @@
+import face_recognition
+import cv2
+
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    face_locations = face_recognition.face_locations(rgb_small_frame)
+
+    
+
+    for (top, right, bottom, left) in face_locations:
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
