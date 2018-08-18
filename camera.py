6210e34b5a59d740c51aba3bf9645b896dd63368
Song Jaehak <master@hpi.cc>
2018-08-18 14:46:07 +0800

    Arrange code

--- camera.py
+++ camera.py
@@ -2,28 +2,29 @@ import face_recognition
 import cv2
 
 
+FRAME_SKIP = 1
+RESIZE = 0.3
+
 video_capture = cv2.VideoCapture(0)
+frame_skip_counter = 0
+face_locations = None
 
 while True:
     ret, frame = video_capture.read()
 
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    if frame_skip_counter == 0:
+        rgb_frame = cv2.resize(frame, (0, 0), fx=RESIZE, fy=RESIZE)[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_frame)
+        frame_skip_counter = FRAME_SKIP
 
-    rgb_small_frame = small_frame[:, :, ::-1]
-
-    face_locations = face_recognition.face_locations(rgb_small_frame)
-
-    
-
-    for (top, right, bottom, left) in face_locations:
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+    for loc in face_locations:
+        (top, right, bottom, left) = map(lambda x: int(x / RESIZE),  loc)
 
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
     cv2.imshow('Video', frame)
+    
+    frame_skip_counter -= 1
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
